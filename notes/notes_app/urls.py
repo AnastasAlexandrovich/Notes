6bee9b20d131dea1cb3618d4e60a91ab54f18af9
Anastasiia Khoroshilova <anastasiia.khoroshilova@netcracker.com>
2020-12-21 00:25:05 +0400

    add login and registration

--- notes/notes_app/urls.py
+++ notes/notes_app/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.template.backends import django
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('index', views.index),
-]
+    path('register', views.register, name='register'),
+    path('login', views.signin, name='login')
+    ]
--- notes/notes_app/views.py
+++ notes/notes_app/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import SignupForm, LoginForm
 
 
 def index(request):
     return render(request, 'notes_app/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=email, password=password)
+            login(request, user)
+            return redirect('notes_app/index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def signin(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(username=email, password=password)
+            if user is not None:
+                login(request, user)
+            else:
+                #если не зареган
+                return redirect('/notes_app')
+            return redirect('index')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
